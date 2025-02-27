7fc726cd8379b258ac7ddf80723725f36d26a645
Nathaniel Coleman <kyloren@turtleshelldesigns.com>
2025-02-27 13:14:46 -0500

    Work on QR Code generator

---
+++ qr_code_generator/qr_code_generator.py
@@ -0,0 +1,10 @@
+# Program to generate an QR Code from user entered text or URL
+import qrcode
+
+user_data = input("Enter the text or URL: ")
+qr_code_filename = input("Enter the filename: ")
+
+img = qrcode.make(user_data)
+type(img)
+img.save(qr_code_filename)
+print(f"QR code saved as {qr_code_filename}")
